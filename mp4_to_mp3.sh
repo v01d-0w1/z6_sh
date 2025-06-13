248a1b2792f770ef518a310afde556a4f83c543d
z6death <tnz426.z6@gmail.com>
2025-06-13 17:22:02 +0600

    initial

--- mp4_to_mp3.sh
+++ mp4_to_mp3.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 cd ~/Music/
-for f in *.mp4; do ffmpeg -i "$f" -vn -ab 192k -ar 44100 -y "${f%.mp4}.mp3"; done
-rm *.mp4
+
+#for f in *.mp4; do ffmpeg -i "$f" -vn -ab 192k -ar 44100 -y "${f%.mp4}.mp3"; done
+shopt -s nocaseglob
+for f in *.{mp4,mkv,avi,mov,webm,flv,wmv,m4a,wav,aac,ogg,flac}; do
+  [ -f "$f" ] || continue
+  ffmpeg -i "$f" -vn -ab 192k -ar 44100 -y "${f%.*}.mp3"
+done
+shopt -u nocaseglob
+
+shopt -s extglob
+rm -f !(*.mp3)
 
