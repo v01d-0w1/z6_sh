fe621cacdeb6f4ba60a17f2579eb9593f9458c98
z6death <tnz426.z6@gmail.com>
2025-12-24 15:26:20 +0600

    initial

--- git_set.sh
+++ git_set.sh
@@ -4,14 +4,14 @@ echo "create <1> or update <2>"
 
 read -p "choose:" state
 
-echo "path of the repo(home/z6/your path)"
+echo "path of the repo(home/z6/git_repo/your path)"
 read path
 
 if [ $state = 1 ]; then
 
     echo "url"
     read url
-    cd /home/z6/$path
+    cd /home/z6/git_repo/$path
     git init
     git add .
     git commit -m "Initial commit"
