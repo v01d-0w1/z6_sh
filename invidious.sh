36d5ec3dc93fa61546aeadafe0e7e02165f191be
z6 <tnz426.z6@gmail.com>
2026-02-27 18:04:24 +0600

    Initial commit

---
+++ invidious.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd ~/invidious || exit
+
+if docker compose ps --format json 2>/dev/null | grep -q '"Name":.*invidious.*"State":"running"'; then
+    # Stop Invidious
+    sudo docker compose down
+    pkill -f "qutebrowser.*3000"
+    notify-send "Invidious" "Stopped" -t 1000
+else
+    # Start Invidious
+    sudo docker compose up -d
+    notify-send "Invidious" "Starting..." -t 1000
+    sleep 3
+    
+    # Launch qutebrowser in a completely detached way
+    (setsid qutebrowser http://localhost:3000 >/dev/null 2>&1 &)
+fi
+
+# Give it a moment to launch
+sleep 1
+exit
